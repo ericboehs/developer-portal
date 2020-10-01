cdf96537a819e83338c71778dfae9f124ceb4bdd
Anthony Kopczyk <anthony.kopczyk@oddball.io>
2020-10-01 10:53:44 -0400

    Organize code as much as possible
    I tried to organize the code to make it a little more readable. That
    said I think we could improve this further. At the same time, it might
    be best to leave this for later, as the quickest fix is to use location
    react hooks, but we can't use those until we get react router upgraded
    to a newer version.

--- src/components/PageContent.tsx
+++ src/components/PageContent.tsx
@@ -2,6 +2,14 @@ import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 import { SiteRoutes } from '../Routes';
 
+const focusAndScroll = (elementToFocus?: HTMLElement | null) => {
+  if (elementToFocus) {
+    elementToFocus.focus();
+  }
+
+  window.scrollTo(0, 0);
+};
+
 const PageContent = (props: RouteComponentProps): JSX.Element => {
   const mainRef = React.useRef<HTMLElement>(null);
   const prevLocationRef = React.useRef<string | null>(null);
@@ -10,16 +18,10 @@ const PageContent = (props: RouteComponentProps): JSX.Element => {
 
   React.useEffect(() => {
     const currentPath = location.pathname && location.hash;
+    const prevLocation: string | null = prevLocationRef.current;
 
-    if (prevLocationRef?.current) {
-      if (prevLocationRef.current === currentPath) {
-        return;
-      }
-
-      if (mainRef?.current) {
-        mainRef.current.focus();
-      }
-      window.scrollTo(0, 0);
+    if (prevLocation && prevLocation !== currentPath) {
+      focusAndScroll(mainRef?.current);
     }
 
     prevLocationRef.current = currentPath;
