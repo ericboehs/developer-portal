2e4b070510508d2aba928dfb98a1410182bf8c14
Jennifer Nguy <jennifer.nguy@libertyits.com>
2020-10-05 10:52:04 -0400

    Changed number of APi categories

--- src/apiDefs/query.test.ts
+++ src/apiDefs/query.test.ts
@@ -2,23 +2,23 @@
   This file contains unit tests for the query.ts module in this directory. Because query.ts
   is the only module that accesses data from the Typescript definition files in data/ directly,
   we cannot currently mock the dependencies of these functions. Because query is a core module for
-  this application, however, it is better not to leave these functions untested. Therefore, this 
+  this application, however, it is better not to leave these functions untested. Therefore, this
   test suite tests high-level results of these functions by querying our real data. When the source
   of that data changes from Typescript objects to a database, we can mock the calls to the database
-  instead of checking real data. 
-  
+  instead of checking real data.
+
   In the meantime, these tests are limited to checking properties of our APIs that should be relatively
   stable over time, so that they will not break for the majority of pull requests to developer-portal.
-  If you make a significant change to one of the APIs, you may need to update these tests, but that 
+  If you make a significant change to one of the APIs, you may need to update these tests, but that
   should be a rare occurrence. If you need to add tests to this file, please respect this convention
   and avoid testing with data that is likely to change over time.
 */
 
 import 'jest';
-import { 
+import {
   includesOauthAPI,
-  lookupApiByFragment, 
-  lookupApiCategory, 
+  lookupApiByFragment,
+  lookupApiCategory,
 } from './query';
 
 describe('query module', () => {
@@ -58,17 +58,17 @@ describe('query module', () => {
       expect(benefitsApi).not.toBeNull();
       expect(benefitsApi!.apis.length).toBeGreaterThanOrEqual(3);
       expect(benefitsApi!.apis.map(api => !!api.oAuth).filter(m => m).length).toEqual(1);
-      
+
       const facilitiesApi = lookupApiCategory('facilities');
       expect(facilitiesApi).not.toBeNull();
       expect(facilitiesApi!.apis.length).toBeGreaterThanOrEqual(1);
       expect(facilitiesApi!.apis.map(api => !!api.oAuth).filter(m => m).length).toEqual(0);
-      
+
       const healthApi = lookupApiCategory('health');
       expect(healthApi).not.toBeNull();
-      expect(healthApi!.apis.length).toBeGreaterThanOrEqual(4);
-      expect(healthApi!.apis.map(api => !!api.oAuth).filter(m => m).length).toEqual(4);
-      
+      expect(healthApi!.apis.length).toBeGreaterThanOrEqual(3);
+      expect(healthApi!.apis.map(api => !!api.oAuth).filter(m => m).length).toEqual(3);
+
       const verificationApi = lookupApiCategory('verification');
       expect(verificationApi).not.toBeNull();
       expect(verificationApi!.apis.length).toBeGreaterThanOrEqual(3);
@@ -85,7 +85,7 @@ describe('query module', () => {
     it('returns true if the list includes an API within a key-based category', () => {
       expect(includesOauthAPI(['benefits', 'facilities', 'fhir'])).toBe(true);
     });
-    
+
     it('returns true if the list includes an API that is marked as OAuth at the API level', () => {
       expect(includesOauthAPI(['benefits', 'claims', 'facilities'])).toBe(true);
     });
